0ad0f60d147f02d030bfb27858a0169cff538b52
goodideas <goodideas@goodideasde-MacBook-Pro.local>
2020-05-07 18:25:34 +0800

    MainActivity add Properties

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -5,9 +5,17 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    val ct = this
+
+    val list = mutableListOf<Int>()
+
+    val adapter = Adapter(list,ct)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 
 }
